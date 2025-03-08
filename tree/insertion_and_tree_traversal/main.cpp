116faae24b347aa20304cff9fe094511409cd5e4
fuad <nafisfuadisc@gmail.com>
2025-03-08 20:27:46 +0600

    BST

---
+++ tree/insertion_and_tree_traversal/main.cpp
@@ -0,0 +1,172 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    node* left;
+    node* right;
+
+    node(int value, node* left = nullptr, node* right = nullptr)
+        : data(value), left(left), right(right) {}
+};
+node* root = NULL;
+
+void preorder(const node* temp)
+{
+    if (temp)
+    {
+        cout << temp->data << ' ';
+        preorder(temp->left);
+        preorder(temp->right);
+    }
+}
+
+void inorder(const node* temp)
+{
+    if (temp)
+    {
+        inorder(temp->left);
+        cout << temp->data << ' ';
+        inorder(temp->right);
+    }
+}
+
+void postorder(const node* temp)
+{
+    if (temp)
+    {
+        postorder(temp->left);
+        postorder(temp->right);
+        cout << temp->data << ' ';
+    }
+}
+
+void bst_insert(const vector<int>& elements)
+{
+    for (int data : elements)
+    {
+        node* temp = new node(data);
+        if (!root) root = temp;
+        else
+        {
+            node *curr_node = root;
+            node *prev_node = nullptr;
+            while(curr_node)               // goto the correct pos
+            {
+                prev_node = curr_node;
+                if(curr_node->data > data) // data smaller, goto left
+                    curr_node = curr_node->left;
+                else                       // data greater or equal, goto right
+                    curr_node = curr_node->right;
+            }
+            if (prev_node->data > data)
+                prev_node->left = temp;
+            else
+                prev_node->right = temp;
+        }
+    }
+}
+
+void bst_delete(int data)
+{
+    node* curr_node = root;
+    node* prev_node = nullptr;
+
+    // find the node to delete
+    while (curr_node)
+    {
+        if (curr_node->data == data) break;
+        prev_node = curr_node;
+        if(curr_node->data > data) // data smaller, goto left
+            curr_node = curr_node->left;
+        else                       // data greater or equal, goto right
+            curr_node = curr_node->right;
+    }
+
+    // node not found
+    if (!curr_node) return;
+
+    // case 1: no child
+    else if (!curr_node->left && !curr_node->right)
+    {
+        if (!prev_node) // deleting root
+            root = nullptr;
+        if(prev_node->left == curr_node)
+            prev_node->left = nullptr;
+        else
+            prev_node->right = nullptr;
+
+        delete curr_node;
+    }
+
+    // case 2: only child
+    else if(!curr_node->left || !curr_node->right)
+    {
+        // find child
+        node* child = curr_node->left 
+            ? curr_node->left 
+            : curr_node->right;
+
+        // set child
+        if (!prev_node)  // deleting root
+            root = child;
+        else if(prev_node->left == curr_node)
+            prev_node->left = child;
+        else
+            prev_node->right = child;
+
+        delete curr_node;
+    }
+
+    // case 3: two children (inorder successor: find leftmost node in the right subtree)
+    else
+    {
+        node* successor = curr_node->right;
+        node* successor_parent = curr_node;
+
+        while (successor->left) // find leftmost node in right subtree
+        {
+            successor_parent = successor;
+            successor = successor->left;
+        }
+
+        // replace data
+        curr_node->data = successor->data;
+
+        // delete successor
+        if (successor_parent->left == successor)  // if (successor_parent == curr_node) then it must be checked
+            successor_parent->left = successor->right;
+        else
+            successor_parent->right = successor->right;
+
+        delete successor;
+    }
+}
+
+int main()
+{
+    /* without insert()
+    // node *a, *b, *c, *d, *e;
+    // a = new node(50);
+    // b = new node(75);
+    // c = new node(60, a, b);
+    
+    
+    // a = new node(90);
+    // b = new node(104);
+    // d = new node(100, a, b);
+    
+    // e = new node(81, c, d);
+    // root = e;
+    */
+    ///* with insert()
+    bst_insert({81,60,100,50,75,90,104});
+    //*/
+
+    bst_delete(81);
+
+    preorder(root); cout << endl;
+    // inorder(root); cout << endl;
+    // postorder(root); cout << endl;
+}
